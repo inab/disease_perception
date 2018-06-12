31fc47a8de4feb316a7a9f85c4aca6fb7040521a
José María Fernández <jose.m.fernandez@bsc.es>
2018-06-12 18:36:19 +0200

    Nodes and edges in cytoscape format are now computed on fetch
    Also, store the JSON data instead of the promise of the JSON data

--- FRONTEND/app/diseases.js
+++ FRONTEND/app/diseases.js
@@ -1,7 +1,17 @@
 // Singleton variables
 var _Diseases;
+var _DiseaseNodes;
+
 var _DiseaseGroups;
+var _DiseaseGroupNodes;
+
 var _DiseaseComorbiditiesNetwork;
+
+var _DiseaseComorbiditiesNetworkMinRisk;
+var _DiseaseComorbiditiesNetworkMaxRisk;
+var _DiseaseComorbiditiesNetworkInitialCutoff;
+
+var _DiseaseComorbiditiesNetworkEdges;
 
 export class Diseases {
 	constructor(cyContainer) {
@@ -17,7 +27,25 @@ export class Diseases {
 			fetchPromises.push(
 				fetch('api/diseases', {mode: 'no-cors'})
 				.then(function(res) {
-					_Diseases = res.json();
+					return res.json();
+				})
+				.then(function(decodedJson) {
+					_Diseases = decodedJson;
+					_DiseaseNodes = _Diseases.map(function(dis) {
+						let retdis = {
+							// jshint ignore:start
+							...dis
+							// jshint ignore:end
+						};
+						// Unique identifiers
+						retdis.id = 'D'+dis.id;
+						// jshint camelcase: false 
+						retdis.parent = 'DG'+dis.disease_group_id;
+						delete retdis.disease_group_id;
+						return {
+							data: retdis
+						};
+					});
 					return _Diseases;
 				})
 			);
@@ -27,7 +55,23 @@ export class Diseases {
 			fetchPromises.push(
 				fetch('api/diseases/groups', {mode: 'no-cors'})
 				.then(function(res) {
-					_DiseaseGroups = res.json();
+					return res.json();
+				})
+				.then(function(decodedJson) {
+					_DiseaseGroups = decodedJson;
+					_DiseaseGroupNodes = _DiseaseGroups.map(function(dg) {
+						let retdg = {
+							// jshint ignore:start
+							...dg
+							// jshint ignore:end
+						};
+						// Unique identifiers
+						retdg.id = 'DG'+dg.id;
+						// jshint camelcase: false 
+						return {
+							data: retdg
+						};
+					});
 					return _DiseaseGroups;
 				})
 			);
@@ -37,7 +81,50 @@ export class Diseases {
 			fetchPromises.push(
 				fetch('api/diseases/comorbidities', {mode: 'no-cors'})
 				.then(function(res) {
-					_DiseaseComorbiditiesNetwork = res.json();
+					return res.json();
+				})
+				.then(function(decodedJson) {
+					_DiseaseComorbiditiesNetwork = decodedJson;
+					
+					_DiseaseComorbiditiesNetworkEdges = _DiseaseComorbiditiesNetwork.map(function(dc,dci) {
+						// Preparation
+						let retdc = {
+							// jshint ignore:start
+							...dc
+							// jshint ignore:end
+						};
+						// jshint camelcase: false 
+						// Unique identifiers
+						retdc.id = 'DC'+dci;
+						retdc.source = 'D'+dc.from_id;
+						retdc.target = 'D'+dc.to_id;
+						delete retdc.from_id;
+						delete retdc.to_id;
+						
+						return {
+							data: retdc
+						};
+					});
+					
+					let minRisk = Infinity;
+					let maxRisk = -Infinity;
+					let initialCutoff = -Infinity;
+					if(_DiseaseComorbiditiesNetwork.length > 0) {
+						let dcme = [..._DiseaseComorbiditiesNetwork];
+						// jshint camelcase: false 
+						dcme.sort(function(e1,e2) { return e1.rel_risk - e2.rel_risk; });
+						minRisk = dcme[0].rel_risk;
+						maxRisk = dcme[dcme.length - 1].rel_risk;
+						
+						// Selecting the initial cutoff risk, based on the then biggest values
+						initialCutoff = dcme[Math.floor(dcme.length * 9 / 10)].rel_risk;
+					}
+					
+					// Saving the range and cutoff for later processing
+					_DiseaseComorbiditiesNetworkMinRisk = minRisk;
+					_DiseaseComorbiditiesNetworkMaxRisk = maxRisk;
+					_DiseaseComorbiditiesNetworkInitialCutoff = initialCutoff;
+					
 					return _DiseaseComorbiditiesNetwork;
 				})
 			);
@@ -52,5 +139,21 @@ export class Diseases {
 	
 	getComorbiditiesNetwork() {
 		return _DiseaseComorbiditiesNetwork;
+	}
+	
+	getRelRiskRange() {
+		return [_DiseaseComorbiditiesNetworkMinRisk,_DiseaseComorbiditiesNetworkInitialCutoff,_DiseaseComorbiditiesNetworkMaxRisk];
+	}
+	
+	getCYComorbiditiesNetwork() {
+		return {
+			nodes: [
+				// jshint ignore:start
+				..._DiseaseNodes,
+				..._DiseaseGroupNodes
+				// jshint ignore:end
+			],
+			edges: _DiseaseComorbiditiesNetworkEdges
+		};
 	}
 }
