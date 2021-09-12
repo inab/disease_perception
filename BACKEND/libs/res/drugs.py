9ce9606a43165b91f38110adbdf209cfd6528f95
José María Fernández <jose.m.fernandez@bsc.es>
2021-09-12 15:15:25 +0200

    Now original drugs paths are rewritten based on redirections to new REST API

--- BACKEND/libs/res/drugs.py
+++ BACKEND/libs/res/drugs.py
@@ -4,31 +4,33 @@
 
 import sys, os
 
-from .api_models import CMResPath, CMRoutes, CMResource, DRUGS_NS, drug_model
+from .api_models import CMResPath, CMRoutes, CMResource, DRUGS_NS
+
+from flask import redirect
+
+from .nodes import NodesDetailed, NodesByName
 
 class DrugList(CMResource):
 	'''Shows a list of all the drugs related in comorbidity studies'''
 	@DRUGS_NS.doc('list_drugs')
-	@DRUGS_NS.marshal_list_with(drug_model)
 	def get(self):
 		'''List all drugs involved in the different studies'''
-		return self.cmn.drugs()
+		return redirect(self.api.url_for(NodesDetailed, h_id=self.default_h_id, n_type='drug'))
 
 @DRUGS_NS.response(404, 'Drug not found')
 @DRUGS_NS.param('id', 'The drug id')
 class Drug(CMResource):
 	'''Return the detailed information of a drug'''
 	@DRUGS_NS.doc('drug')
-	@DRUGS_NS.marshal_with(drug_model)
 	def get(self,id):
 		'''It gets detailed drug information'''
-		return self.cmn.drug(id)
+		return redirect(self.api.url_for(NodesByName, h_id=self.default_h_id, n_type='drug', name=id))
 
 ROUTES = CMRoutes(
 	ns=DRUGS_NS,
 	path='/drugs',
 	routes=[
 		CMResPath(DrugList,''),
-		CMResPath(Drug,'/<int:id>')
+		CMResPath(Drug,'/<string:id>')
 	]
 )
