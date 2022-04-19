54e980be8e5ee6594498ae0fcb34444d061d7596
José María Fernández <jose.m.fernandez@bsc.es>
2022-04-19 10:41:36 +0200

    Unfinished work translating old routes to new ones through redirects

--- BACKEND/libs/res/patients.py
+++ BACKEND/libs/res/patients.py
@@ -10,33 +10,34 @@ from .api_models import CMResPath, CMRoutes, CMResource, \
     patient_subgroup_intersect_drugs_model, patient_map_genes_model, \
     patient_map_drugs_model, patients_interaction_model
 
+from flask import redirect
+
+from .nodes import NodesDetailed, NodesByName, EdgesFromNodesByName
+
 class PatientList(CMResource):
-	'''Shows a list of all the patient subgroups'''
+	'''Shows a list of all the patients'''
 	@PATIENT_NS.doc('list_patients')
-	@PATIENT_NS.marshal_list_with(patient_model)
 	def get(self):
 		'''List all the patients present in the comorbidity network'''
-		return self.cmn.patients()
+		return redirect(self.api.url_for(NodesDetailed, h_id=self.default_h_id, n_type='patient'))
 
 @PATIENT_NS.response(404, 'Patient not found')
 @PATIENT_NS.param('id', 'The patient id')
 class Patient(CMResource):
 	'''Return the detailed information of a patient'''
 	@PATIENT_NS.doc('patient')
-	@PATIENT_NS.marshal_with(patient_model)
-	def get(self,id):
+	def get(self, id:str):
 		'''It gets detailed patient information'''
-		return self.cmn.patient(patient_id=id)
+		return redirect(self.api.url_for(NodesByName, h_id=self.default_h_id, n_type='patient', name=id))
 
 @PATIENT_NS.response(404, 'No patient was found')
 @PATIENT_NS.param('ids', 'The patient id(s), separated by commas')
 class PatientsMapGenes(CMResource):
 	'''Return the list of genes which map with the queried patients'''
 	@PATIENT_NS.doc('patient_genes')
-	@PATIENT_NS.marshal_list_with(patient_map_genes_model)
-	def get(self,ids):
+	def get(self,ids:str):
 		'''It gets the mapped genes for each queried patient'''
-		return self.cmn.patient_map_genes(patient_ids=ids)
+		return redirect(self.api.url_for(EdgesFromNodesByName, h_id=self.default_h_id, n_type='patient', e_type='patient_gene_maps', name=ids))
 
 @PATIENT_NS.response(400, 'The number of different patient ids must be at least two')
 @PATIENT_NS.response(404, 'No patient was found')
@@ -44,10 +45,9 @@ class PatientsMapGenes(CMResource):
 class PatientsMapDrugs(CMResource):
 	'''Return the list of drugs which map with the queried patients'''
 	@PATIENT_NS.doc('patient_drugs')
-	@PATIENT_NS.marshal_list_with(patient_map_drugs_model)
 	def get(self,ids):
 		'''It gets the mapped drugs for each queried patient'''
-		return self.cmn.patient_map_drugs(patient_ids=ids)
+		return redirect(self.api.url_for(EdgesFromNodesByName, h_id=self.default_h_id, n_type='patient', e_type='patient_drug_maps', name=ids))
 
 @PATIENT_NS.response(404, 'No patient was found')
 @PATIENT_NS.param('ids', 'The patient id(s), separated by commas')
@@ -137,23 +137,30 @@ class PatientSubgroupPatientsInteraction(CMResource):
 		'''It gets the interactions among the patients of the query patient subgroup'''
 		return self.cmn.patients_interactions(patient_subgroup_ids=ids)
 
+class PatientTest(CMResource):
+	def get(self, ruta):
+		return {
+			'input': ruta
+		}
+
 ROUTES = CMRoutes(
 	ns=PATIENT_NS,
 	path='/patients',
 	routes=[
 		CMResPath(PatientList,''),
 		CMResPath(Patient,'/<int:id>'),
-		CMResPath(PatientsMapGenes,'/<list(int,sep=","):ids>/genes'),
-		CMResPath(PatientsMapDrugs,'/<list(int,sep=","):ids>/drugs'),
-		CMResPath(PatientsInteraction,'/<list(int,sep=","):ids>/interaction'),
+		CMResPath(PatientTest, '/test/<splitpath(sub=list,subtype=int,sep=","):ruta>/b'),
+		CMResPath(PatientsMapGenes,'/<list(string,sep=","):ids>/genes'),
+		CMResPath(PatientsMapDrugs,'/<list(string,sep=","):ids>/drugs'),
+		CMResPath(PatientsInteraction,'/<list(string,sep=","):ids>/interaction'),
 		CMResPath(PatientSubgroupList,'/subgroups'),
 		CMResPath(PatientSubgroupPatients,'/subgroups/<int:id>'),
 		CMResPath(PatientSubgroupPatients,'/subgroups/<int:id>/patients'),
-		CMResPath(PatientSubgroupsIntersectGenes,'/subgroups/<list(int,sep=","):ids>/genes'),
-		CMResPath(PatientSubgroupsIntersectDrugs,'/subgroups/<list(int,sep=","):ids>/drugs'),
-		CMResPath(PatientSubgroupPatientsMapGenes,'/subgroups/<list(int,sep=","):ids>/patients/genes'),
-		CMResPath(PatientSubgroupPatientsMapDrugs,'/subgroups/<list(int,sep=","):ids>/patients/drugs'),
-		CMResPath(PatientSubgroupPatientsInteraction,'/subgroups/<list(int,sep=","):ids>/patients/interaction'),
+		CMResPath(PatientSubgroupsIntersectGenes,'/subgroups/<list(string,sep=","):ids>/genes'),
+		CMResPath(PatientSubgroupsIntersectDrugs,'/subgroups/<list(string,sep=","):ids>/drugs'),
+		CMResPath(PatientSubgroupPatientsMapGenes,'/subgroups/<list(string,sep=","):ids>/patients/genes'),
+		CMResPath(PatientSubgroupPatientsMapDrugs,'/subgroups/<list(string,sep=","):ids>/patients/drugs'),
+		CMResPath(PatientSubgroupPatientsInteraction,'/subgroups/<list(string,sep=","):ids>/patients/interaction'),
 		CMResPath(PatientSubgroup,'/subgroups/<int:id>/info'),
 	]
 )
